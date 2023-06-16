72a486a43eddfcadee4c5d3898d80b5624a100f4
Karina Zubko <karinazubko2017@gmail.com>
2023-06-16 16:18:50 +0300

    Add Kotlin code and project settings

--- Bounty-board/src/main/kotlin/Main.kt
+++ Bounty-board/src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    //task 0
+    var hasSteed: Boolean = false
+    var numOfCoins: Int = 50
+    //task 2
+    val HERO_NAME: String = "Madrigal"
+    var temp_str = ""
+    HERO_NAME.forEach { temp ->
+        temp_str = temp+temp_str
+    }
+    println(temp_str)
 }
+/*
+* Kotlin REPL(как jshell) - Read, Evaluate, Print, Loop (прочитать, выполнить, вывести, повторить).
+* исходный(.kt)->байт-код jvm->выполнение
+* var/val - camelCase
+* const val - SNAKE_CASE - константа времени компиляции(string, float, byte, int, long, char, double, short, boolean)
+* List - коллекция
+* Set -  коллекция уникальных
+* Map -  колекция пар "ключ-значение"
+* Kotlin поддерживает только ref типы
+*
+* */

6cc0fc0f70b77f60bc58fe6180b69bde6406f174
Karina Zubko <karinazubko2017@gmail.com>
2023-06-25 15:10:11 +0300

    a simple example of lambda expressions

--- Functional_programming_and_collections/src/main/kotlin/Main.kt
+++ Functional_programming_and_collections/src/main/kotlin/Main.kt
@@ -4,15 +4,23 @@ fun main(args: Array<String>) {
     //после count - литерал функции
     println("KarinaZubko".count({ letter -> letter == 'k' }))
     Narration.narrate("A hero exters the woen of Kronstadt. What is their name?")
-    //a simple example of lambda expressions 
-    val printHello: () -> Unit = { println("Hello")}
+    //a simple example of lambda expressions
+    val printHello: () -> Unit = {println("Hello")}
     printHello()
+    val sum:(Int, Int) -> Int = { a, b ->
+        a+b
+    }
+    println("${sum(3,4675)}")
 }
 /*
 * fun - именованные функции
 * анонимные функции - лямбда-функции
 * Alonzo Church 1930 - лямбда-исчисления
-*
+* val lambdaName: (param1: Type1, param2: Type2, ...) -> ReturnType = { arg1, arg2, ... ->
+*   //тело функции
+*   //..
+*   //возращяемое значение
+* }
 *
 *
 *
